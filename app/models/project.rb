6d4eb6b9a20a065cb633fe82651b3474a9a798a5
Zymna <62969568+Zymna@users.noreply.github.com>
2024-10-10 14:20:08 +0200

    fix: attempt optimize EXISTS subqueries
    MySQL8 engine seems to struggle with an EXISTS subquery that is used in
    most views of redmine.
    
    https://dev.mysql.com/doc/refman/8.4/en/subquery-optimization-with-exists.html
    Seems to indicate we can help the optimizer by including the outer
    condition in the inner query.
    
    In particular this quote:
    > A very useful optimization is to “inform” the subquery that the only
    > rows of interest are those where the inner expression inner_expr is
    > equal to outer_expr. This is done by pushing down an appropriate
    > equality into the subquery's WHERE clause to make it more restrictive.
    
    This would be an example diff of the produced SQL:
    ```diff
    SELECT COUNT(*)
    FROM `issues`
    INNER JOIN `projects` ON `projects`.`id` = `issues`.`project_id`
    INNER JOIN `issue_statuses` ON `issue_statuses`.`id` = `issues`.`status_id`
    WHERE (
        projects.status <> 9
        AND EXISTS (
            SELECT 1 AS one
            FROM enabled_modules em
            WHERE em.project_id = projects.id
            AND em.name='issue_tracking'
    +       projects.status <> 9
        )
    )
    AND ((
        issues.status_id IN (
            SELECT id FROM issue_statuses WHERE is_closed=FALSE
        ))
    )
    ```

--- app/models/project.rb
+++ app/models/project.rb
@@ -179,7 +179,8 @@ class Project < ActiveRecord::Base
     base_statement = (perm && perm.read? ? "#{Project.table_name}.status <> #{Project::STATUS_ARCHIVED}" : "#{Project.table_name}.status = #{Project::STATUS_ACTIVE}")
     if !options[:skip_pre_condition] && perm && perm.project_module
       # If the permission belongs to a project module, make sure the module is enabled
-      base_statement << " AND EXISTS (SELECT 1 AS one FROM #{EnabledModule.table_name} em WHERE em.project_id = #{Project.table_name}.id AND em.name='#{perm.project_module}')"
+      # Optimised based on https://dev.mysql.com/doc/refman/8.4/en/subquery-optimization-with-exists.html
+      base_statement = " AND EXISTS (SELECT 1 AS one FROM #{EnabledModule.table_name} em WHERE em.project_id = #{Project.table_name}.id AND em.name='#{perm.project_module}' AND #{base_statement})"
     end
     if project = options[:project]
       project_statement = project.project_condition(options[:with_subprojects])
